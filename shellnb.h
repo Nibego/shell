b9f02d6e1cebbbbb1796a6a19327fc43a0c05892
Nibego <nicolas.bejarano@correounivalle.edu.co>
2018-05-16 23:54:25 -0500

    Add files via upload

---
+++ shellnb.h
@@ -0,0 +1,10 @@
+#ifndef __SHELLNB__
+#define __SHELLNB__
+
+//Definicion de funciones auxiliares para el shell
+void sinEspacios(char* comando);
+int pHijo();
+int saveFile();
+int redireccion();
+
+#endif
